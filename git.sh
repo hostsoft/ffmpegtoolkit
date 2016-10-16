c573979b74a1cb55c3d231836699abd8f2ccdcbd
Ninetian Inc <chenhui11@mails.jlu.edu.cn>
2016-10-16 10:21:12 +0800

    update to git 2.10.1 versions

--- git.sh
+++ git.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-#FFMPEG installation script
-
-#  Copyright (C) 2007-2014 Sherin.co.in. All rights reserved.
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 RED='\033[01;31m'
 RESET='\033[0m'
 INSTALL_SDIR='/usr/src/ffmpegscript'
@@ -36,9 +19,9 @@ if [ -e "/usr/bin/git" ]; then
 else
 	cd $INSTALL_SDIR/
 	rm -rf git*
-	wget $SOURCE_URL/git-1.7.2.5.tar.gz
-	tar -xzf git-1.7.2.5.tar.gz
-	cd git-1.7.2.5/
+	wget https://github.com/git/git/archive/v2.10.1.tar.gz
+	tar -xzf git-2.10.1.tar.gz
+	cd git-2.10.1/
 	./configure --prefix=/usr/
 	make -j$cpu
 	make install
