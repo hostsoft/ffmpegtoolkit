446acbde5862f11f0b48bee4f8ca01179ad7e2ff
Ninetian Inc <chenhui11@mails.jlu.edu.cn>
2016-10-16 14:49:46 +0800

    Update mp4box.sh

--- mp4box.sh
+++ mp4box.sh
@@ -2,11 +2,11 @@
 RED='\033[01;31m'
 RESET='\033[0m'
 INSTALL_SDIR='/usr/src/ffmpegscript'
-SOURCE_URL='http://mirror.ffmpeginstaller.com/source/gpac'
+SOURCE_URL='https://github.com/gpac/gpac/archive'
 INSTALL_DDIR='/usr/local/cpffmpeg'
 export cpu=`cat "/proc/cpuinfo" | grep "processor"|wc -l`
 export TMPDIR=$HOME/tmp
-_package='gpac-full-0.5.0.tar.gz'
+_package='v0.6.1.tar.gz'
 clear
 sleep 2
 echo -e $RED"Installation of $_package ....... started"$RESET
