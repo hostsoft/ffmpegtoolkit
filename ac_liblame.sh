232fb8d9ca311b886c9181345cb2feafbb51fd35
Ninetian Inc <chenhui11@mails.jlu.edu.cn>
2018-04-02 13:38:33 +0800

    back down version

--- ac_liblame.sh
+++ ac_liblame.sh
@@ -2,7 +2,7 @@
 
 function _install_lame() {
 	clear
-	_file="lame-3.100.tar.gz"
+	_file="lame-3.99.5.tar.gz"
 	_package="Lame MP3 Lib"
 	echo -e $RED"Installation of $_package ....... started"$RESET
 
@@ -12,11 +12,12 @@ function _install_lame() {
 		echo "$_file found, Skip Downloads"
 	else
 		echo "$_file not found, Try Downloading......"
+		#https://ftp.osuosl.org/pub/blfs/conglomeration/lame/
 	        wget https://ftp.osuosl.org/pub/blfs/conglomeration/lame/$_file
 	fi
 
 	tar -zxvf $_file
-	cd lame-3.100/
+	cd lame-3.99.5/
 	./configure --prefix=$INSTALL_DIR --enable-mp3x --enable-mp3rtp
 	make -j $cpu
 	make install
