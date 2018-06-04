aec271d467ae5429befe5e119a8d834c41e5923b
Spiro g <spirogg@users.noreply.github.com>
2018-06-04 03:38:12 -0500

    Update vc_ffmpeg.sh

--- vc_ffmpeg.sh
+++ vc_ffmpeg.sh
@@ -17,15 +17,11 @@ function _install_ffmpeg() {
 	./configure --prefix=$INSTALL_DIR \
 	--pkg-config-flags="--static" \
 	--extra-libs=-lpthread \
-	--enable-gpl --disable-stripping --enable-avresample --enable-avisynth --enable-gnutls --enable-ladspa \
-	--enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio --enable-libebur128 \
-	--enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libgme \
-	--enable-libgsm --enable-libmp3lame --enable-libopenjpeg --enable-libopenmpt --enable-libopus \
-	--enable-libpulse --enable-librubberband --enable-libshine --enable-libsnappy --enable-libsoxr \
-	--enable-libspeex --enable-libssh --enable-libtheora --enable-libtwolame --enable-libvorbis --enable-libvpx \
-	--enable-libwavpack --enable-libwebp --enable-libx265 --enable-libxvid --enable-libzmq --enable-libzvbi \
-	--enable-omx --enable-openal --enable-opengl --enable-sdl2 --enable-libdc1394 --enable-libiec61883 \
-	--enable-chromaprint --enable-frei0r --enable-libopencv --enable-libx264 --enable-shared \
+	--enable-gpl --enable-shared --enable-nonfree \
+	--enable-pthreads  --enable-libopencore-amrnb --enable-libopencore-amrwb \
+	--enable-libmp3lame --enable-libvpx --enable-libfdk-aac --enable-libfreetype \
+	--enable-libtheora --enable-libvorbis  --enable-libx264 --enable-libx265 --enable-libxvid \
+	--enable-postproc --enable-swscale --enable-avfilter --enable-libass --enable-runtime-cpudetect \
 	--extra-cflags=-I/usr/local/ffmpegtoolkit/include/ --extra-ldflags=-L/usr/local/ffmpegtoolkit/lib \
 	--enable-version3
 	make -j $cpu
