9a517b8f9e7e8bf3d5e67ea0cba5daa60558f3ba
Ninetian Inc <chenhui11@mails.jlu.edu.cn>
2018-07-20 20:34:34 +0800

    update value

--- vc_aom.sh
+++ vc_aom.sh
@@ -7,10 +7,13 @@ function _install_aom() {
 	echo -e $RED"Installation of $_package ....... started"$RESET
 	cd $SOURCE_DIR/
 	[ -d "aom" ] && rm -rf "aom"
-  git clone https://aomedia.googlesource.com/aom && \
-  cmake3 ./aom && \
-  make && \
-  make install
+	git -C aom pull 2> /dev/null || git clone --depth 1 https://aomedia.googlesource.com/aom
+	mkdir aom_build
+	cd aom_build
+	PATH="$HOME/bin:$PATH" 
+	cmake3 -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DENABLE_SHARED=off -DENABLE_NASM=on ../aom
+	make
+	make install
 	echo -e $RED"Installation of $_package ....... Completed"$RESET
 	cd $SOURCE_DIR/
 }
